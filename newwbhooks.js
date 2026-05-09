4845229876d8ae45e5fe3c92e7a66b88e5232be2
GHlm2651 <t920s00@outlook.com>
2026-05-09 01:44:24 -0700

    Create newwbhooks.js

---
+++ newwbhooks.js
@@ -0,0 +1,6 @@
+{
+  "webhooks" : {
+    "games" : "eLV4gLI2H}=oe\\Jnf7NoHiJrfW=lgCor`6RmeC=re7IrIPQsInQ0I~o}IP]4J~U}I~Q5IG=1QR=BH\\tW^o=TR@MsJSJJeAVOR~`1JPJ6Q\\BBKPJOHR`kfo5TQiJSI5c4gPVvJ\\JG^45eKSM7R5c}aQQ0]S5BPi^VHU99",
+    "unrGames" : "eLV4gLI2H}=oe\\Jnf7NoHiJrfW=lgCor`6RmeC=re7IrIPQsInQ7I~]}InQ0J@M7KPA}IG=vSo|p]oh^KBBII4|HgRlm]61rg~^7RSNPJSB0`A1je\\^1WPhmg4tPg@lhKBJRUQ1PU~B0Vo1OfjVGIj]6SnFOgi`n^c99"
+  }
+}
